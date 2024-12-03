8764f7fbe11d1d034266e0219b37ae0cc41b228b
ivan.abramov <ivan.abramov@onetwotrip.com>
2024-12-03 15:58:39 +0300

    add new tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -2,21 +2,33 @@ import { Post, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
+
   const post: Omit<Post, 'id' | 'date'> = {
     text: 'Mocked post',
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
-
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({text: 'Some pre-existing post'});
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const newPost = postsService.create(post);
+
+    expect(newPost).toMatchObject({
+      text: post.text,
+      id: '2',
+      date: expect.any(String),
+    });
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    const post = postsService.find('1');
+
+    expect(post).toMatchObject({
+      id: '1',
+      text: 'Some pre-existing post',
+      date: expect.any(String),
+    });
   });
 });
